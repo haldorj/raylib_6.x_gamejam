8b0e58d4f9ecf723f649b08c246d35707363f7f1
Haldor Johansen <haldorjh@gmail.com>
2026-07-07 14:26:30 +0200

    Flat top hexagons axial coordinates

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,8 +1,6 @@
 #include "pch.h"
 
-#include <charconv>
-#include <ios>
-#include <ranges>
+#include <format>
 
 #ifdef PLATFORM_WEB
 #include <emscripten/emscripten.h>      // Emscripten library
@@ -12,34 +10,38 @@
 
 namespace hexagon
 {
-    // Pointy top orientation
+    // Flat top orientation
 
     // Measurements
-    constexpr auto Size{8};
-    constexpr auto Width{static_cast<int>(std::numbers::sqrt3 * Size)};
-    constexpr auto Height{2 * Size};
-    // Distances
-    constexpr auto Horizontal{static_cast<int>(Width)};
-    constexpr auto Vertical{static_cast<int>(Height * 0.75)};
+    constexpr auto Size{8.0};
+    constexpr auto Width{2.0 * Size};
+    constexpr auto Height{std::numbers::sqrt3 * Size};
 
-    Vector2 HexToPixel(const int row, const int column)
+    // Distances
+    constexpr auto Horizontal{Width * 0.75};
+    constexpr auto Vertical{Height};
+
+    static Vector2 HexToPixel(const int row, const int column)
     {
-        const auto even{row % 2 == 1};
-        const auto x = std::numbers::sqrt3 * (column + (even ? 0.5 : 0.0));
-        const auto y = (3.0 / 2 * row);
-        // scale cartesian coordinates
-        const auto result{Vector2{static_cast<float>(x * Size), static_cast<float>(y * Size)}};
-        return result;
+        // Hex to cartesian
+        const auto x{3.0 / 2 * static_cast<double>(column)};
+        const auto y{std::numbers::sqrt3 / 2.0 * column + std::numbers::sqrt3 * row};
+        // Scale cartesian coordinates
+        const auto result = Vector2{
+            .x = static_cast<float>(x * Size),
+            .y = static_cast<float>(y * Size)
+        };
+        return result; 
     }
 
-    constexpr Vector2 Points[] = {
-        Vector2{.x = static_cast<float>(Width / 2.0), .y = Height},
-        Vector2{.x = static_cast<float>(Width), .y = Height * 0.75},
-        Vector2{.x = static_cast<float>(Width), .y = Height * 0.25},
-        Vector2{.x = static_cast<float>(Width / 2), .y = 0},
-        Vector2{.x = 0, .y = Height * 0.25},
-        Vector2{.x = 0, .y = Height * 0.75},
-    };
+    // constexpr Vector2 Points[] = {
+    //     Vector2{.x = static_cast<float>(Width / 2.0), .y = Height},
+    //     Vector2{.x = static_cast<float>(Width), .y = Height * 0.75},
+    //     Vector2{.x = static_cast<float>(Width), .y = Height * 0.25},
+    //     Vector2{.x = static_cast<float>(Width / 2), .y = 0},
+    //     Vector2{.x = 0, .y = Height * 0.25},
+    //     Vector2{.x = 0, .y = Height * 0.75},
+    // };
 }
 
 namespace
@@ -53,8 +55,6 @@ namespace
 
     Texture2D hexagon;
 
-    RenderTexture2D renderTexture;
-
     void Init()
     {
 #ifndef _DEBUG
@@ -63,17 +63,14 @@ namespace
         InitWindow(WindowWidth, WindowHeight, "raylib gamejam template");
         SetTargetFPS(TargetFps);
 
-        hexagon = LoadTexture("assets/textures/hex.png");
+        hexagon = LoadTexture("assets/textures/flathex.png");
 
+        camera.offset = {.x = static_cast<float>(WindowWidth) / 2.f, .y = static_cast<float>(WindowHeight) / 2.f};
         camera.zoom = static_cast<float>(GetScreenHeight()) / GamePixelHeight;
-
-        renderTexture = LoadRenderTexture(WindowWidth, WindowHeight);
-        SetTextureFilter(renderTexture.texture, TEXTURE_FILTER_POINT);
     }
 
     void Shutdown()
     {
-        UnloadRenderTexture(renderTexture);
         CloseWindow();
     }
 
@@ -94,67 +91,25 @@ namespace
         //----------------------------------------------------------------------------------
         // Render game screen to a texture, 
         // it could be useful for scaling or further shader postprocessing
-        BeginTextureMode(renderTexture);
-        ClearBackground(SKYBLUE);
+        //BeginTextureMode(renderTexture);
 
-        // TODO: Draw your game screen here
-
-        // const auto drawHexgon = [](const int centerX, const int centerY, const Color color)
-        // {
-        //     for (size_t startIndex = 0; startIndex < std::size(hexagon::Points); ++startIndex)
-        //     {
-        //         size_t endIndex = {startIndex + 1};
-        //         if (startIndex + 1 >= std::size(hexagon::Points))
-        //         {
-        //             endIndex = 0;
-        //         }
-        //         const auto& [startX, startY] = hexagon::Points[startIndex];
-        //         const auto& [endX, endY] = hexagon::Points[endIndex];
-        //
-        //         DrawLine(centerX + static_cast<int>(startX), centerY + static_cast<int>(startY),
-        //                  centerX + static_cast<int>(endX), centerY + static_cast<int>(endY),
-        //                  color);
-        //     }
-        // };
-
-        for (const auto row : std::views::iota(0, 12))
-        {
-            for (const auto col : std::views::iota(0, 14))
-            {
-                DrawTextureEx(hexagon, hexagon::HexToPixel(row, col), 0.f, 1.0f, WHITE);
-            }
-        }
-
-        DrawText("HEXAGONS", 10, 10, 10, DARKGRAY);
-
-        //
-        EndTextureMode();
-
-        // Render to screen (main framebuffer)
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
 
         BeginMode2D(camera);
+        
+        // TODO: Draw your game screen here
 
-        // Draw render texture to screen, scaled if required
-        const auto source{
-            Rectangle{
-                .x = 0,
-                .y = 0,
-                .width = static_cast<float>(renderTexture.texture.width),
-                .height = -static_cast<float>(renderTexture.texture.height)
+        for (const auto row : std::views::iota(-5, 5))
+        {
+            for (const auto col : std::views::iota(-5, 5))
+            {
+                const auto pos{hexagon::HexToPixel(row, col)};
+                const auto str{std::format("{}, {}", row, col)};
+                DrawTextureEx(hexagon, pos, 0.f, 1.0f, WHITE);
+                //DrawText(str.c_str(), static_cast<int>(pos.x), static_cast<int>(pos.y), 1, RED);
             }
-        };
-        const auto dest{
-            Rectangle{
-                .x = 0, .y = 0,
-                .width = static_cast<float>(renderTexture.texture.width),
-                .height = static_cast<float>(renderTexture.texture.height)
-            }
-        };
-        constexpr auto origin{Vector2{.x = 0, .y = 0}};
-        constexpr auto rotation{0.0f};
-        DrawTexturePro(renderTexture.texture, source, dest, origin, rotation, WHITE);
+        }
 
         EndMode2D();
 
