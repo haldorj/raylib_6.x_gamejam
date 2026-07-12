ab2231152670d797e2ec6ebeda0f2d0ebd861f16
Haldor Johansen <haldorjh@gmail.com>
2026-07-12 17:35:38 +0200

    removed comments

--- assets/shaders/shader_web.fs
+++ assets/shaders/shader_web.fs
@@ -2,15 +2,12 @@
 
 precision mediump float;
 
-// Interpolated from the vertex shader
 varying vec2 fragTexCoord;
 varying vec4 fragColor;
 
-// Uniforms
 uniform sampler2D texture0;
 uniform vec4 colDiffuse;
 
-// Screen dimensions
 const float renderWidth = 720.0;
 const float renderHeight = 720.0;
 
@@ -18,17 +15,10 @@ const float renderHeight = 720.0;
 
 void main()
 {
-    // Convert UV from [0,1] to [-1,1]
     vec2 p = fragTexCoord * 2.0 - 1.0;
-
-    // Fisheye distortion
     float curvature = 0.02;
     p *= 1.0 + curvature * dot(p, p);
-
-    // Convert back to texture coordinates
     vec2 uv = p * 0.485 + 0.5;
-
-    // Outside screen -> transparent
     if (uv.x < 0.0 || uv.x > 1.0 ||
         uv.y < 0.0 || uv.y > 1.0)
     {
@@ -36,18 +26,12 @@ void main()
         return;
     }
 
-    // Sample texture
     vec4 texelColor = texture2D(texture0, uv);
-
-    // Slight CRT tint
     texelColor *= vec4(0.85, 0.80, 0.95, 1.0);
-
-    // Scanlines
     float frequency = renderHeight * 0.5;
     float scanline = 1.0 + 0.15 * cos(uv.y * frequency * PI);
 
     texelColor *= scanline;
 
-    // Final output
     gl_FragColor = texelColor * colDiffuse * fragColor;
 }
